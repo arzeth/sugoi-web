dc2bf6521a93284e144999d36ac09cd198cbc14b
Arzet Ro <arzeth0@gmail.com>
2022-03-21 20:08:49 +0500

    Don't enable deduplication by default

--- htlDb.php
+++ htlDb.php
@@ -11,18 +11,18 @@ Source code: https://github.com/arzeth/sugoi-web
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 {
-    if (isset($_SERVER['HTTP_REFERER']) && !isset($_SERVER['HTTP_ORIGIN']))
-    {
-        if (preg_match('@^https?://[^/]+@i', $_SERVER['HTTP_REFERER'], $matches))
-        {
-            $_SERVER['HTTP_ORIGIN'] = $matches[0];
-        }
-    }
+	if (isset($_SERVER['HTTP_REFERER']) && !isset($_SERVER['HTTP_ORIGIN']))
+	{
+		if (preg_match('@^https?://[^/]+@i', $_SERVER['HTTP_REFERER'], $matches))
+		{
+			$_SERVER['HTTP_ORIGIN'] = $matches[0];
+		}
+	}
 }
 header('Access-Control-Allow-Origin: ' . (@$_SERVER['HTTP_ORIGIN'] ?: '*'));
 header('Access-Control-Allow-Methods: GET, PUT, POST');
 header('Access-Control-Allow-Credentials: True');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Pragma, Cache-Control, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 header('Vary: Origin');
 $name = @$_GET['name'] ?: @$_GET['title'] ?: @$_GET['game'] ?: @$_GET['vn'] ?: @$_GET['VN'] ?: 'natsukoi';
 if (!preg_match('/^[a-zA-Z0-9_\\-]+$/', $name)) die('only -_a-zA-Z0-9 are allowed in ?name=');
@@ -36,230 +36,235 @@ $IS_READONLY_MODE = str_starts_with(@$_POST['fn'], 'get');
 $dbFilePath = __DIR__ . '/' . 'htr__' . $name  . '.db';
 if ($IS_READONLY_MODE && !is_readable($dbFilePath))
 {
-    die('null');
+	die('null');
 }
 $db = new SQLite3(
-	'htr__' . $name  . '.db',
-    $IS_READONLY_MODE
-    ? SQLITE3_OPEN_READONLY
+	$dbFilePath,
+	$IS_READONLY_MODE
+	? SQLITE3_OPEN_READONLY
 	: SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE
 );
 $possibleLangs = [
-    'am',
-    'ar',
-    'arz',
-    'be',
-    'bn',
-    'cs',
-    'da',
-    'de',
-    'el',
-    'en',
-    'eo',
-    'es',
-    'fa',
-    'fi',
-    'fr',
-    'he',
-    'hi',
-    'hu',
-    'hy',
-    'id',
-    'interslv',
-    'it',
-    'ja',
-    'jbo',
-    'jv',
-    'ka',
-    'ko',
-    'la',
-    'lt',
-    'ms',
-    'nl',
-    'no',
-    'pl',
-    'pt_BR',
-    'pt_PT',
-    'qu',
-    'ro',
-    'ru',
-    'sa',
-    'sl',
-    'sr',
-    'sv',
-    'sw',
-    'ta',
-    'th',
-    'tl',
-    'tr',
-    'tt',
-    'uk',
-    'vi',
-    'yue',
-    'zh_Hans',
-    'zh_Hant',
+	'am',
+	'ar',
+	'arz',
+	'be',
+	'bn',
+	'cs',
+	'da',
+	'de',
+	'el',
+	'en',
+	'eo',
+	'es',
+	'fa',
+	'fi',
+	'fr',
+	'he',
+	'hi',
+	'hu',
+	'hy',
+	'id',
+	'interslv',
+	'it',
+	'ja',
+	'jbo',
+	'jv',
+	'ka',
+	'ko',
+	'la',
+	'lt',
+	'ms',
+	'nl',
+	'no',
+	'pl',
+	'pt_BR',
+	'pt_PT',
+	'qu',
+	'ro',
+	'ru',
+	'sa',
+	'sl',
+	'sr',
+	'sv',
+	'sw',
+	'ta',
+	'th',
+	'tl',
+	'tr',
+	'tt',
+	'uk',
+	'vi',
+	'yue',
+	'zh_Hans',
+	'zh_Hant',
 ];
 
 if ($IS_READONLY_MODE)
 {
-    if ($db->query(<<<SQL
+	if ($db->query(<<<SQL
 SELECT COUNT(*)
 FROM pragma_table_info('$name')
 SQL
-    )->fetchArray(SQLITE3_NUM)[0] === 0)
-    {
-        die('null');
-    }
+	)->fetchArray(SQLITE3_NUM)[0] === 0)
+	{
+		die('null');
+	}
 }
 else
 {
-    $db->query(<<<SQL
+	$db->query(<<<SQL
 CREATE TABLE IF NOT EXISTS "$name" (
-    "sourceTxt" TEXT NOT NULL,
-    "sourceId" CHAR(12) NOT NULL,
-    "recordCreatedOrUpdatedOn" UNSIGNED BIGINT(13) NOT NULL,
-    PRIMARY KEY(sourceId)
+	"sourceTxt" TEXT NOT NULL,
+	"sourceId" CHAR(12) NOT NULL,
+	"recordCreatedOrUpdatedOn" UNSIGNED BIGINT(13) NOT NULL,
+	PRIMARY KEY(sourceId)
 )
 SQL
-    );
-    $db->query(<<<SQL
+	);
+	$db->query(<<<SQL
 CREATE INDEX IF NOT EXISTS sourceTxt_idx on "$name" (sourceTxt)
 SQL
-    );
+	);
 }
 
 if (
-    (
-        @$_POST['fn'] === 'getBySourceTxt'
-        ||
-        @$_POST['fn'] === 'getBySourceId'
-        ||
-        @$_POST['fn'] === 'getBySourceIdOrSourceTxt'
-    )
+	(
+		@$_POST['fn'] === 'getBySourceTxt'
+		||
+		@$_POST['fn'] === 'getBySourceId'
+		||
+		@$_POST['fn'] === 'getBySourceIdOrSourceTxt'
+	)
 )
 {
-    $whichLangsToGet = (
-        $_POST['whichLangsToGet'] === '*' || $_POST['whichLangsToGet'] === 'all'
-        ? true
-        : $_POST['whichLangsToGet']// json array
-    );
+	$whichLangsToGet = (
+		$_POST['whichLangsToGet'] === '*' || $_POST['whichLangsToGet'] === 'all'
+		? true
+		: $_POST['whichLangsToGet']// json array
+	);
 	$sourceTxt = @$_POST['sourceTxt'];
-    $sourceId = @$_POST['sourceId'];
-    if ($_POST['fn'] === 'getBySourceTxt' || $_POST['fn'] === 'getBySourceIdOrSourceTxt')
-    {
-        $query = $db->prepare(<<<SQL
-SELECT * FROM "$name"
-WHERE sourceTxt=:sourceTxt
-SQL
-        );
-        $query->bindValue(':sourceTxt', $sourceTxt);
-        $ret = $query->execute();
-        $ret = $ret->fetchArray(SQLITE3_ASSOC);
-    }
-    if ($_POST['fn'] === 'getBySourceId' || ($_POST['fn'] === 'getBySourceIdOrSourceTxt' && $ret === false))
-    {
-        $query = $db->prepare(<<<SQL
+	$sourceId = @$_POST['sourceId'];
+	if ($_POST['fn'] === 'getBySourceId' || $_POST['fn'] === 'getBySourceIdOrSourceTxt')
+	{
+		$query = $db->prepare(<<<SQL
 SELECT * FROM "$name"
 WHERE sourceId=:sourceId
 SQL
-        );
-        $query->bindValue(':sourceId', $sourceId);
-        $ret = $query->execute();
-        $ret = $ret->fetchArray(SQLITE3_ASSOC);
-    }
+		);
+		$query->bindValue(':sourceId', $sourceId);
+		$ret = $query->execute();
+		$ret = $ret->fetchArray(SQLITE3_ASSOC);
+	}
+	if ($_POST['fn'] === 'getBySourceTxt' || ($_POST['fn'] === 'getBySourceIdOrSourceTxt' && $ret === false))
+	{
+		$query = $db->prepare(<<<SQL
+SELECT * FROM "$name"
+WHERE sourceTxt=:sourceTxt
+SQL
+		);
+		$query->bindValue(':sourceTxt', $sourceTxt);
+		$ret = $query->execute();
+		$ret = $ret->fetchArray(SQLITE3_ASSOC);
+	}
 
-    if ($ret === false)
-    {
-        die('null');
-    }
-    if ($whichLangsToGet === true)
-    {
-        unset($ret['sourceId']);
-        unset($ret['sourceTxt']);
-        unset($ret['recordCreatedOrUpdatedOn']);
-    }
-    else
-    {
-        foreach ($ret as $col => $value)
-        {
-            if (!in_array($col, $whichLangsToGet, true)) unset($ret[$col]);
-        }
-    }
+	if ($ret === false)
+	{
+		die('null');
+	}
+	if ($whichLangsToGet === true)
+	{
+		unset($ret['sourceId']);
+		unset($ret['sourceTxt']);
+		unset($ret['recordCreatedOrUpdatedOn']);
+	}
+	else
+	{
+		foreach ($ret as $col => $value)
+		{
+			if (!in_array($col, $whichLangsToGet, true)) unset($ret[$col]);
+		}
+	}
 
-    die(json_encode($ret, JSON_UNESCAPED_UNICODE));
+	die(json_encode($ret, JSON_UNESCAPED_UNICODE));
 }
 
 elseif (
-    @$_POST['fn'] === 'set'
+	@$_POST['fn'] === 'set'
 )
 {
-    if (
-        !isset($_POST['sourceTxt']) ||
-        !isset($_POST['sourceId']) ||
-        !isset($_POST['recordCreatedOrUpdatedOn']) ||
-        !isset($_POST['resultLang']) ||
-        !isset($_POST['resultTxt'])
-    )
-    {
-        die('not all request fields are set');
-    }
-    //todo sanitize resultLang
-    $sourceTxt = $_POST['sourceTxt'];
-    $sourceId = $_POST['sourceId'];
-    $recordCreatedOrUpdatedOn = (int)$_POST['recordCreatedOrUpdatedOn'];
-    $resultLang = $_POST['resultLang'];
-    $resultTxt = $_POST['resultTxt'];
+	if (
+		!isset($_POST['sourceTxt']) ||
+		!isset($_POST['sourceId']) ||
+		!isset($_POST['recordCreatedOrUpdatedOn']) ||
+		!isset($_POST['resultLang']) ||
+		!isset($_POST['resultTxt'])
+	)
+	{
+		die('not all request fields are set');
+	}
+	//todo sanitize resultLang
+	$sourceTxt = $_POST['sourceTxt'];
+	$sourceId = $_POST['sourceId'];
+	$recordCreatedOrUpdatedOn = (int)$_POST['recordCreatedOrUpdatedOn'];
+	$resultLang = $_POST['resultLang'];
+	$resultTxt = $_POST['resultTxt'];
 
-    if ($db->query(<<<SQL
+	if ($db->query(<<<SQL
 SELECT COUNT(*)
 FROM pragma_table_info('$name')
 WHERE "name"="$resultLang"
 SQL
-    )->fetchArray(SQLITE3_NUM)[0] !== 1)
-    {
-        $db->query(<<<SQL
-    ALTER TABLE "$name"
-    ADD "$resultLang" TEXT
+	)->fetchArray(SQLITE3_NUM)[0] !== 1)
+	{
+		$db->query(<<<SQL
+	ALTER TABLE "$name"
+	ADD "$resultLang" TEXT
 SQL
-        );
-    }
+		);
+	}
 
-    //$db->enableExceptions(true);
-    $query = $db->prepare(<<<SQL
+	//$db->enableExceptions(true);
+	$query = $db->prepare(<<<SQL
 UPDATE OR IGNORE "$name"
 SET
-    "recordCreatedOrUpdatedOn"=:recordCreatedOrUpdatedOn,
-    "$resultLang"=:$resultLang
+	"recordCreatedOrUpdatedOn"=:recordCreatedOrUpdatedOn,
+	"$resultLang"=:$resultLang
 WHERE
-    "sourceId"=:sourceId
+	"sourceId"=:sourceId
 SQL
-    );
-    $query->bindValue(':recordCreatedOrUpdatedOn', $recordCreatedOrUpdatedOn, SQLITE3_INTEGER);
-    $query->bindValue(":$resultLang", $resultTxt, SQLITE3_TEXT);
-    //$query->bindValue(':sourceTxt', $sourceTxt);
-    $query->bindValue(':sourceId', $sourceId, SQLITE3_TEXT);
-    $query->execute();
+	);
+	$query->bindValue(':recordCreatedOrUpdatedOn', $recordCreatedOrUpdatedOn, SQLITE3_INTEGER);
+	$query->bindValue(":$resultLang", $resultTxt, SQLITE3_TEXT);
+	//$query->bindValue(':sourceTxt', $sourceTxt);
+	$query->bindValue(':sourceId', $sourceId, SQLITE3_TEXT);
+	$query->execute();
 
-    $query = $db->prepare(<<<SQL
+	$query = $db->prepare(<<<SQL
 INSERT OR IGNORE INTO "$name"
-     ("sourceTxt", "sourceId", "recordCreatedOrUpdatedOn", "$resultLang")
+	 ("sourceTxt", "sourceId", "recordCreatedOrUpdatedOn", "$resultLang")
 VALUES (:sourceTxt, :sourceId, :recordCreatedOrUpdatedOn, :$resultLang)
 SQL
-    );
-    $query->bindValue(':sourceTxt', $sourceTxt, SQLITE3_TEXT);
-    $query->bindValue(':sourceId', $sourceId, SQLITE3_TEXT);
-    $query->bindValue(':recordCreatedOrUpdatedOn', $recordCreatedOrUpdatedOn, SQLITE3_INTEGER);
-    $query->bindValue(":$resultLang", $resultTxt, SQLITE3_TEXT);
-    $query->execute();
+	);
+	$query->bindValue(':sourceTxt', $sourceTxt, SQLITE3_TEXT);
+	$query->bindValue(':sourceId', $sourceId, SQLITE3_TEXT);
+	$query->bindValue(':recordCreatedOrUpdatedOn', $recordCreatedOrUpdatedOn, SQLITE3_INTEGER);
+	$query->bindValue(":$resultLang", $resultTxt, SQLITE3_TEXT);
+	$query->execute();
 
 
-    $query = $db->prepare(<<<SQL
+	if (1) // debugging
+	{
+		$query = $db->prepare(<<<SQL
 SELECT * FROM "$name"
+WHERE sourceId=:sourceId
 SQL
-        );
-        $query->bindValue(':sourceTxt', $sourceTxt);
-        $ret = $query->execute();
-        $ret = $ret->fetchArray(SQLITE3_ASSOC);
-    die(var_export($ret, true));
+		);
+		$query->bindValue(':sourceId', $sourceId, SQLITE3_TEXT);
+		//$query->bindValue(':sourceTxt', $sourceTxt);
+		$ret = $query->execute();
+		$ret = $ret->fetchArray(SQLITE3_ASSOC);
+		die(var_export($ret, true));
+	}
 }
 $db->close();
